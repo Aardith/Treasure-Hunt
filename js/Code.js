6cbc61ede76b7564aa6467176e5e9a55067d4ff0
Aardith <77149224+Aardith@users.noreply.github.com>
2021-07-17 15:41:38 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+     
+    fill("white")
+    textSize(15)
+    text("N N F I C T O U", 400,250)
+    fill("lightblue")
+    text("Hint: Is a set of instruction!!", 400,270)
+
+    fill("white")
+    textSize(15)
+    text("E S D T A A A B", 700,150)
+    fill("lightblue")
+    text("Hint: Done on Firebase!!", 700,170)
+// add code for displaying rest of the names and the hints.
+    
+
+}
